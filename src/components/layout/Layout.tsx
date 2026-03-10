ee7bb65a12ae40940bf8a8a67c3e5f222725f184
Simon Buechi <simon.buechi@gmail.com>
2026-03-10 21:12:41 +0100

    add offline view, serviceworker

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,14 +3,18 @@ import { Box, Container } from '@mui/material';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import MobileNavigation from './MobileNavigation';
+import { useOnlineStatus } from '../../hooks/useOnlineStatus';
+import OfflineView from '../common/OfflineView';
 
 const Layout = () => {
+    const isOnline = useOnlineStatus();
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Navbar />
             <Box component="main" sx={{ flexGrow: 1, py: 4, pb: { xs: 10, md: 4 } }}>
                 <Container maxWidth="lg">
-                    <Outlet />
+                    {isOnline ? <Outlet /> : <OfflineView />}
                 </Container>
             </Box>
             <Footer />
